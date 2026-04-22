3d4b3dab9e8a83696096a0671d942c5736d208bc
Praveen Roy <pkroy0505@gmail.com>
2026-04-22 08:18:30 +0530

    added playwright

--- Typescript/Employees.ts
+++ Typescript/Employees.ts
@@ -62,7 +62,7 @@ Static - It can be accessed without creating an instance of the class. It is sha
 // import export
 
 export class Employees{
-    public empid:number
+    empid:number
     empName:string
     private empSalary:string
     protected empcity:string
@@ -70,6 +70,8 @@ export class Employees{
     static companyName:string = "Microsoft"
 
     constructor(id:number, name:string, sal:string, city:string, offer:number){
+        // this keyword - It refers to the current object. It is used to access the properties and methods of the current object.
+
         this.empid = id // emp1.empid = 101
         this.empName = name //emp1.empName = "John"
         this.empSalary = sal // emp1.empSalary = "50000"
@@ -77,14 +79,58 @@ export class Employees{
         this.offer = offer // emp1.offer = 10000
     }
 
+    employeeInformation(){
+        console.log("Employee ID: ", this.empid);
+        console.log("Employee Name: ", this.empName);
+        console.log("Employee Salary: ", this.empSalary);
+        console.log("Employee City: ", this.empcity);
+        console.log("Employee Offer: ", this.offer);
+        console.log("Company Name: ", Employees.companyName);
+    }
+
+    // static method - It can be accessed without creating an instance of the class. It is shared by all the instances of the class. It can be accessed using the class name. Inside static method we cannot use "this" keyword to access the properties and methods of the class. We can only access the static properties and methods of the class using the class name.
+
+
+    static companyInformation(){
+        console.log("Company Name: ", Employees.companyName); 
+    }
+
 }
 
 
 const emp = new Employees(101, "John", "50000", "New York", 10000)
 console.log(emp.empName);
+emp.empName = "Jyoti"
+console.log(Employees.companyName);
+emp.employeeInformation()
+Employees.companyInformation()
+// emp.empSalary
+// emp.offer = 15000
 
-const emp1 = new Employees(101, "Rahul", "500000", "Delhi", 200000)
+
+const emp1 = new Employees(102, "Rahul", "500000", "Delhi", 200000)
 console.log(emp1.empName);
-console.log(emp1.empSalary);
+console.log(Employees.companyName);
+emp1.employeeInformation()
+Employees.companyInformation()
 
 
+
+
+
+
+
+
+/*
+
+1. nodejs - npm
+2. Editor - Visual Studio Code
+3. Typescript - npm install -g typescript
+4. tsx - typescript executor - tsx filename.ts
+5. playwright installation - npm init playwright
+
+
+
+
+
+*/
